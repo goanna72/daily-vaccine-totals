fb400b9b247dc6900f32b9bfbc1f6b6b9d6eae34
Russ <sayersr@amazon.com>
2021-10-20 20:00:36 +1100

    moved everything out of if statements

--- daily_vaccine_total_by_state/app.py
+++ daily_vaccine_total_by_state/app.py
@@ -5,6 +5,17 @@ from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
 
+
+state_fullname = {
+    "VIC" : "Victoria",
+    "WA" : "Western Australia",
+    "SA" : "South Australia",
+    "TAS" : "Tasmania",
+    "NSW" : "New South Wales",
+    "NT" : "Northern Territory",
+    "ACT" : "Australian Capital Territory"
+}
+
 class LaunchRequestHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_request_type("LaunchRequest")(handler_input)
@@ -32,114 +43,22 @@ class VaccineIntentHandler(AbstractRequestHandler):
         state = handler_input.request_envelope.request.intent.slots['states'] \
             .resolutions.resolutions_per_authority[0].values[0].value.id
 
-        try:
-            dyndb = boto3.resource('dynamodb', region_name='ap-southeast-2')
-            table = dyndb.Table('Vaccines')
-            if state.upper() == "VIC":
-                #data = table.get_item(Key={'MeasureName': "VIC - Administration state - Daily increase doses recorded"})
-                pop = table.get_item(Key={'MeasureName': "VIC - Population 16 and over"})
-                result2 = pop['Item']
-                statepop = result2['Total']
-                vaccinepop = table.get_item(Key={'MeasureName': "VIC - Residence state - Number of people 16 and over fully vaccinated"})
-                result3 = vaccinepop['Item']
-                statefullpop = result3['Total']
-                percentage2 = statefullpop / statepop
-                percentage = format(percentage2 * 100, '.2f')
-                data = table.get_item(Key={'MeasureName': "VIC - Administration state - Daily increase doses recorded"})
-                result = data['Item']
-                statename = "Victoria"
-            elif state.upper() == "WA":
-                data = table.get_item(Key={'MeasureName': "WA - Administration state - Daily increase doses recorded"})
-                result = data['Item']
-                pop = table.get_item(Key={'MeasureName': "WA - Population 16 and over"})
-                result2 = pop['Item']
-                statepop = result2['Total']
-                vaccinepop = table.get_item(Key={'MeasureName': "WA - Residence state - Number of people 16 and over fully vaccinated"})
-                result3 = vaccinepop['Item']
-                statefullpop = result3['Total']
-                percentage2 = statefullpop / statepop
-                percentage = format(percentage2 * 100, '.2f')
-                statename = "West Australia"
-            elif state.upper() == "SA":
-                data = table.get_item(Key={'MeasureName': "SA - Administration state - Daily increase doses recorded"})
-                result = data['Item']
-                pop = table.get_item(Key={'MeasureName': "SA - Population 16 and over"})
-                result2 = pop['Item']
-                statepop = result2['Total']
-                vaccinepop = table.get_item(Key={'MeasureName': "SA - Residence state - Number of people 16 and over fully vaccinated"})
-                result3 = vaccinepop['Item']
-                statefullpop = result3['Total']
-                percentage2 = statefullpop / statepop
-                percentage = format(percentage2 * 100, '.2f')
-                statename = "South Australia"
-            elif state.upper() == "TAS":
-                data = table.get_item(Key={'MeasureName': "TAS - Administration state - Daily increase doses recorded"})
-                result = data['Item']
-                pop = table.get_item(Key={'MeasureName': "TAS - Population 16 and over"})
-                result2 = pop['Item']
-                statepop = result2['Total']
-                vaccinepop = table.get_item(Key={'MeasureName': "TAS - Residence state - Number of people 16 and over fully vaccinated"})
-                result3 = vaccinepop['Item']
-                statefullpop = result3['Total']
-                percentage2 = statefullpop / statepop
-                percentage = format(percentage2 * 100, '.2f')
-                statename = "Tasmania"
-            elif state.upper() == "QLD":
-                data = table.get_item(Key={'MeasureName': "QLD - Administration state - Daily increase doses recorded"})
-                result = data['Item']
-                pop = table.get_item(Key={'MeasureName': "QLD - Population 16 and over"})
-                result2 = pop['Item']
-                statepop = result2['Total']
-                vaccinepop = table.get_item(Key={'MeasureName': "QLD - Residence state - Number of people 16 and over fully vaccinated"})
-                result3 = vaccinepop['Item']
-                statefullpop = result3['Total']
-                percentage2 = statefullpop / statepop
-                percentage = format(percentage2 * 100, '.2f')
-                statename = "Queensland"
-            elif state.upper() == "NSW":
-                data = table.get_item(Key={'MeasureName': "NSW - Administration state - Daily increase doses recorded"})
-                result = data['Item']
-                pop = table.get_item(Key={'MeasureName': "NSW - Population 16 and over"})
-                result2 = pop['Item']
-                statepop = result2['Total']
-                vaccinepop = table.get_item(Key={'MeasureName': "NSW - Residence state - Number of people 16 and over fully vaccinated"})
-                result3 = vaccinepop['Item']
-                statefullpop = result3['Total']
-                percentage2 = statefullpop / statepop
-                percentage = format(percentage2 * 100, '.2f')
-                statename = "New South Wales"
-            elif state.upper() == "NT":
-                data = table.get_item(Key={'MeasureName': "NT - Administration state - Daily increase doses recorded"})
-                result = data['Item']
-                pop = table.get_item(Key={'MeasureName': "NT - Population 16 and over"})
-                result2 = pop['Item']
-                statepop = result2['Total']
-                vaccinepop = table.get_item(Key={'MeasureName': "NT - Residence state - Number of people 16 and over fully vaccinated"})
-                result3 = vaccinepop['Item']
-                statefullpop = result3['Total']
-                percentage2 = statefullpop / statepop
-                percentage = format(percentage2 * 100, '.2f')
-                statename = "Northern Territory"
-            elif state.upper() == "ACT":
-                data = table.get_item(Key={'MeasureName': "ACT - Administration state - Daily increase doses recorded"})
-                result = data['Item']
-                pop = table.get_item(Key={'MeasureName': "ACT - Population 16 and over"})
-                result2 = pop['Item']
-                statepop = result2['Total']
-                vaccinepop = table.get_item(Key={'MeasureName': "ACT - Residence state - Number of people 16 and over fully vaccinated"})
-                result3 = vaccinepop['Item']
-                statefullpop = result3['Total']
-                percentage2 = statefullpop / statepop
-                percentage = format(percentage2 * 100, '.2f')
-                statename = "Australian Capital Territory"
+        dyndb = boto3.resource('dynamodb', region_name='ap-southeast-2')
+        table = dyndb.Table('Vaccines')
+        state_upper = state.upper()
+        pop = table.get_item(Key={'MeasureName': f"{state_upper} - Population 16 and over"})
+        result2 = pop['Item']
+        statepop = result2['Total']
+        vaccinepop = table.get_item(Key={'MeasureName': f"{state_upper} - Residence state - Number of people 16 and over fully vaccinated"})
+        result3 = vaccinepop['Item']
+        statefullpop = result3['Total']
+        percentage2 = statefullpop / statepop
+        percentage = format(percentage2 * 100, '.2f')
+        data = table.get_item(Key={'MeasureName': f"{state_upper} - Administration state - Daily increase doses recorded"})
+        result = data['Item']
+        statename = state_fullname[state_upper]
 
-        except BaseException as e:
-            print(e)
-            raise e
-
-        #result = data['Item']
         speech_text = "The daily vaccine totals for " + statename + " is " + str(result['Total']) + ". " + str(percentage) + "% of the eligible " + state + " population are fully vaccinated"
-        #speech_text = "Your vaccine is " + state
         handler_input.response_builder.speak(speech_text).set_should_end_session(False)
         return handler_input.response_builder.response
 
